8ae58db6b8344934a64e469af27a9e8c89b63330
Emanuel Turis <hello@emanuelturis.com>
2024-10-11 12:02:36 +0300

    add htmx example

--- main.go
+++ main.go
@@ -3,14 +3,29 @@ package main
 import (
 	"net/http"
 
+	"github.com/webdevfuel/htmx-empty-state/task"
 	"github.com/webdevfuel/htmx-empty-state/template"
 )
 
 func main() {
 	r := http.NewServeMux()
 	r.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		component := template.HelloWorld()
+	r.HandleFunc("GET /tasks", func(w http.ResponseWriter, r *http.Request) {
+		tasks := task.ListTasks()
+		component := template.EmptyState(tasks)
+		component.Render(r.Context(), w)
+	})
+	r.HandleFunc("DELETE /tasks/{id}", func(w http.ResponseWriter, r *http.Request) {
+		id := r.PathValue("id")
+		task.DeleteTask(id)
+		tasks := task.ListTasks()
+		component := template.Rows(tasks)
+		component.Render(r.Context(), w)
+	})
+	r.HandleFunc("POST /tasks", func(w http.ResponseWriter, r *http.Request) {
+		task.AddTask()
+		tasks := task.ListTasks()
+		component := template.Rows(tasks)
 		component.Render(r.Context(), w)
 	})
 	http.ListenAndServe("localhost:3000", r)
---
+++ task/task.go
@@ -0,0 +1,54 @@
+package task
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type Task struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var tasks []Task = []Task{
+	{
+		ID:   "1ca9eab4-c23b-45a1-9abb-6a8ec1e46baa",
+		Name: "Task 1",
+	},
+	{
+		ID:   "d03e6daa-3cad-4c8e-97d6-2e2fddfc9f8f",
+		Name: "Task 2",
+	},
+	{
+		ID:   "456fc3b7-03a1-4642-9a5d-80d99a93c013",
+		Name: "Task 3",
+	},
+	{
+		ID:   "1dfe9b83-0068-4efc-9160-9a632aa96cfa",
+		Name: "Task 4",
+	},
+}
+
+func ListTasks() []Task {
+	return tasks
+}
+
+func AddTask() {
+	uuid, _ := uuid.NewRandom()
+	task := Task{
+		ID:   uuid.String(),
+		Name: fmt.Sprintf("Task %d", len(tasks)+1),
+	}
+	tasks = append(tasks, task)
+}
+
+func DeleteTask(id string) {
+	var filteredTasks []Task
+	for _, task := range tasks {
+		if task.ID != id {
+			filteredTasks = append(filteredTasks, task)
+		}
+	}
+	tasks = filteredTasks
+}
